35fdc398be78f7986d3081b4593bddc6180cb0e3
Andrew <andrewchang17@hotmail.com>
2018-03-15 01:31:48 -0700

    fileManager

--- node/fileManager.go
+++ node/fileManager.go
@@ -1,6 +1,12 @@
 package node
 
-import ()
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sort"
+)
 
 var Path = "../Data"
 
@@ -18,18 +24,45 @@ type FileInfo struct {
 
 func getChunk(index int, length int, fname string) (Chunk, error) {
 	var chunk Chunk
-	return chunk, nil
+	file, err := os.Open(fname)
+	data := make([]byte, length)
+	_, err = file.ReadAt(data, int64(index))
+
+	if err == nil {
+		chunk.Index = index
+		chunk.Length = length
+		chunk.Data = data
+	}
+	return chunk, err
 }
 
-func combineChunk(Chunks []Chunk) error {
-	return nil
+func combineChunk(Chunks []Chunk) (combined []byte, err error) {
+	sort.Slice(Chunks, func(i, j int) bool {
+		return Chunks[i].Index < Chunks[j].Index
+	})
+	for _, chunk := range Chunks {
+		combined = append(combined, chunk.Data...)
+	}
+	//TODO not sure what to return
+	return combined, nil
 }
 
-func serachFile(fname string) (FileInfo, error) {
+func searchFile(fname string) (FileInfo, error) {
 	var fileInfo FileInfo
-	return fileInfo, nil
+	content, err := ioutil.ReadFile(fname)
+	if err == nil {
+		fileInfo.Fname = fname
+		fileInfo.Size = len(content)
+		fileInfo.Hash = string(content[:])
+	}
+	return fileInfo, err
 }
 
 func changeDir(path string) error {
-	return nil
+	//TODO not sure
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+	dir := filepath.Join(basepath, path)
+	err := os.Chdir(dir)
+	return err
 }
