5002eaf93066d6f6622979451329793f5c6dc70a
SimonPlath <Sp05_@hotmail.com>
2018-03-22 02:46:55 -0700

    stubs for testing crypto and fileManager

---
+++ testing/fileManager_test.go
@@ -0,0 +1,41 @@
+package testing
+
+import (
+	"testing"
+)
+
+func TestwriteFile(t *testing.T){
+
+}
+
+func TestdoneWriting(t *testing.T) {
+
+}
+
+func TestcheckHash(t *testing.T) {
+
+}
+
+func TestgetChunk(t *testing.T) {
+
+}
+
+func TestwriteChunk(t *testing.T) {
+
+}
+
+func TesthashFile(t *testing.T) {
+
+}
+
+func TestsearchFile(t *testing.T) {
+
+}
+
+func TestchangeDir(t *testing.T) {
+
+}
+
+
+
+
