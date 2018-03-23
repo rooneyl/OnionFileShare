8aa4c3c9663a3c8898451ab5ab2af62a10aa7596
sjlee <rooneylee@outlook.com>
2018-03-23 03:07:36 -0700

    crypto

--- node/crypto.go
+++ node/crypto.go
@@ -9,42 +9,6 @@ import (
 	"encoding/json"
 )
 
-// type of PublicKey and PrivateKey should be change
-// i just set that as string to make a stub
-
-func encryptData(data []byte, publicKey string) ([]byte, error) {
-
-	pubKeyBytes, err := hex.DecodeString(publicKey)
-	checkError(err)
-
-	pubKey, err := x509.ParsePKIXPublicKey(pubKeyBytes)
-	checkError(err)
-
-	rsaPubKey := pubKey.(*rsa.PublicKey)
-	label := []byte("orders")
-
-	encryptedData, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, rsaPubKey, data, label)
-	checkError(err)
-
-	return encryptedData, nil
-}
-
-func decryptData(data []byte, privateKey string) ([]byte, error) {
-
-	privKeyBytes, err := hex.DecodeString(privateKey)
-	checkError(err)
-
-	privKey, err := x509.ParsePKCS1PrivateKey(privKeyBytes)
-	checkError(err)
-
-	label := []byte("orders")
-
-	decryptedData, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privKey, data, label)
-	checkError(err)
-
-	return decryptedData, nil
-}
-
 func encryptStruct(struc interface{}, pubByte []byte) ([]byte, error) {
 	data, err := json.Marshal(struc)
 	if err != nil {
